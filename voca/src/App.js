cd83ae2463e29d7878b3e520a10c394649462d1d
hyuk <hyuk@woowahan.com>
2023-05-05 23:59:57 +0900

    study : route

--- voca/src/App.js
+++ voca/src/App.js
@@ -4,16 +4,21 @@ import Header from './component/Header';
 import DayList from './component/DayList';
 import Day from './component/Day';
 
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
  
-  
   return (
-    <div className="App">
-      <Header />
-      <DayList/>
-      <Day/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<DayList/>} />
+          <Route path="/day" element={<Day/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -6,15 +6,18 @@ export default function Day() {
 
 
     return (
-    <table>
-        <tbody>
-            {wordList.map(word => (
-                <tr key={word.id}>
-                    <td>{word.eng}</td>
-                    <td>{word.kor}</td>
-                </tr>
-            ))};    
-        </tbody>
-    </table>
+    <>
+        <h2> Day {day}</h2>
+        <table>
+            <tbody>
+                {wordList.map(word => (
+                    <tr key={word.id}>
+                        <td>{word.eng}</td>
+                        <td>{word.kor}</td>
+                    </tr>
+                ))};    
+            </tbody>
+        </table>
+    </>
     );
 }
