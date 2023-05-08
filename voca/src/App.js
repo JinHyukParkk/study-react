cce2b62c8b6fcd9057f7bbcf09bc200879eddabf
hyuk <hyuk@woowahan.com>
2023-05-09 01:58:11 +0900

    feat : 단어, Day 생성 기능 추가

--- voca/src/App.js
+++ voca/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './component/Header';
 import DayList from './component/DayList';
 import Day from './component/Day';
+import CreateWord from './component/CreateWord';
+import CreateDay from './component/CreateDay';
 
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
 import EmptyPage from './component/EmptyPage';
@@ -17,6 +19,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<DayList/>} />
           <Route path="/day/:day" element={<Day/>} />
+          <Route path="/create_word" element={<CreateWord/>} />
+          <Route path='/create_day' element={<CreateDay/>} />
           <Route path="*" element={<EmptyPage/>} />
         </Routes>
       </div>
