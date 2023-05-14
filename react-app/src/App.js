08bdc87a3253d3fc0cf7d110b69962cabd0448fa
hyuk <hyuk@woowahan.com>
2023-05-14 23:53:27 +0900

    init : 초기 연습 코드

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,22 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1><a href="/">WEB</a></h1>
       </header>
-    </div>
+      <nav>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+          <li><a href="/read/3">js</a></li>
+        </ol>
+      </nav>
+  </div>
   );
 }
 
