296be5f067bfb5bfd18ae540aea858b7cdee7ca6
hyuk <hyuk@woowahan.com>
2023-05-17 02:25:11 +0900

    study : event 처리

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,7 +5,10 @@ function Header(props) {
   console.log(props);
   return (
     <header>
-      <h1><a href="/"></a></h1>
+      <h1><a href="/" onClick={(event) => {
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
     </header>
   );
 }
@@ -14,8 +17,13 @@ function Nav(props) {
   return (
     <nav>
         <ol>
-        {props.topics.map((topic) => {
-           return <li><a href={"/read/"+topic.id}>{topic.title}</a></li>
+        {props.topics.map((topic, index) => {
+           return <li key={topic.id}>
+              <a id={topic.id} href={"/read/"+topic.id} onClick={event => {
+                event.preventDefault();
+                props.onChangeMode(event.target.id);
+              }}>{topic.title}</a>
+            </li>
         })}
         </ol>
       </nav>
@@ -32,16 +40,35 @@ function Article(props) {
 }
 
 function App() {
+
+  function ButtonAdminShortcuts({title, clickEvent}) {
+    return (
+      <button
+        onClick={() => clickEvent}
+      > {title} </button>
+    );
+  }
+  
+  function openNaver() {
+    window.open("http://www.naver.com");
+  }
+
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javasccript', body:'javascript is ...'}
   ];
+
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="WEB" onChangeMode={
+        function(){
+          alert("Header")
+        }
+      }></Header>
+      <Nav topics={topics} onChangeMode={(id) => {alert(id)}}></Nav>
       <Article title="Welcom" body="Hello, WEB"></Article>
+      <ButtonAdminShortcuts title="네이버" clickEvent={openNaver}/>
   </div>
   );
 }  
