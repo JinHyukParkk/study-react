732ca5f80541c965e519dc571fc5a55e6a0912ba
hyuk <hyuk@woowahan.com>
2023-05-26 03:00:07 +0900

    style : var -> let 으로 네이밍 수정

--- react-app/component/Counting.js
+++ react-app/component/Counting.js
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 export default function Counting() {
     const [count, setCount] = useState(0);
     const countRef = useRef(0);
-    let countVar = 0;
+    let countLet = 0;
 
     const renderCount = useRef(1);
 
@@ -20,9 +20,9 @@ export default function Counting() {
         countRef.current = countRef.current + 1;
     }
 
-    const increaseCountVar = () => {
+    const increaseCountLet = () => {
         // Render가 되지 않는다. Render가 되어도 값이 0에서 변하지 않는다.(초기화가 되기 때문에)
-        countVar = countVar + 1;
+        countLet = countLet + 1;
     }
 
     useEffect(() => {
@@ -33,11 +33,11 @@ export default function Counting() {
         <div>
             <h1>CountState : {count}</h1>
             <h1>CountRef : {countRef.current}</h1>
-            <h1>CountVar : {countVar}</h1>
+            <h1>CountLet : {countLet}</h1>
             <button onClick={increaseCountState}>State 올려</button>
             <button onClick={increaseCountRef}>Ref 올려</button>
-            <button onClick={increaseCountVar}>Var 올려</button>
-            
+            <button onClick={increaseCountLet}>Let 올려</button>
+
             <h1>Render Count : {renderCount.current}</h1>
         </div>
     );
