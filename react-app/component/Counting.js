4ee114a6faddb6a667944c2df7049c36bd7bad8e
hyuk <hyuk@woowahan.com>
2023-05-25 23:08:47 +0900

    study : useState, useRef, var 에 랜더링 차이 비교

---
+++ react-app/component/Counting.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+export default function Counting() {
+    const [count, setCount] = useState(0);
+    const countRef = useRef(0);
+    let countVar = 0;
+
+    const renderCount = useRef(1);
+
+    console.log("렌더링...")
+    console.log(countRef);
+
+    const increaseCountState = () => {
+        // Render가 된다.
+        setCount(count + 1);
+    }
+
+    const increaseCountRef = () => {
+        // Render가 되지 않는다.
+        countRef.current = countRef.current + 1;
+    }
+
+    const increaseCountVar = () => {
+        // Render가 되지 않는다. Render가 되어도 값이 0에서 변하지 않는다.(초기화가 되기 때문에)
+        countVar = countVar + 1;
+    }
+
+    useEffect(() => {
+        renderCount.current = renderCount.current + 1;
+    });
+
+    return (
+        <div>
+            <h1>CountState : {count}</h1>
+            <h1>CountRef : {countRef.current}</h1>
+            <h1>CountVar : {countVar}</h1>
+            <button onClick={increaseCountState}>State 올려</button>
+            <button onClick={increaseCountRef}>Ref 올려</button>
+            <button onClick={increaseCountVar}>Var 올려</button>
+            
+            <h1>Render Count : {renderCount.current}</h1>
+        </div>
+    );
+}
